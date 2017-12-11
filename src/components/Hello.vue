24bed30773643621879927b953f2a0b6c80ca4cc
Stefan Stoichev <stefan.stoichev@gmail.com>
2017-12-11 17:31:52 +0000

    initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,226 @@
+<template>
+  <div class="fluid">
+<!-- fluid container
+On hold | Proposal | Discovery | Design | Delivery | QA | Suuport
+-->
+      <div>
+        <draggable element="span" v-model="listPending" :options="dragOptions" :move="onMove" class="list-group" @end="onEnd"> 
+            <transition-group name="no" class="list-group" tag="ul">
+              <li class="list-group-item" v-for="element in listPending" :key="element.order"> 
+                <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+                {{element.name}}
+              </li> 
+            </transition-group>
+        </draggable>
+      </div>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th>Member</th>
+      <th>On hold</th>
+      <th>Proposal</th>
+      <th>Discovery</th>
+      <th>Design</th>
+      <th>Delivery</th>
+      <th>QA</th>
+      <th>Support</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+    <th scope="row">Piers</th>
+    <td class="col-sm-2">
+      <div>
+        <draggable element="span" v-model="list" :options="dragOptions" :move="onMove" class="list-group"> 
+            <transition-group name="no" class="list-group" tag="ul">
+              <li class="list-group-item" v-for="element in list" :key="element.order"> 
+                <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+                {{element.name}}
+              </li> 
+            </transition-group>
+        </draggable>
+      </div>
+    </td>
+    <td class="col-sm-2">
+      <div>
+        <draggable element="span" v-model="list2" :options="dragOptions" :move="onMove" class="list-group"> 
+            <transition-group name="no" class="list-group" tag="ul">
+              <li class="list-group-item" v-for="element in list2" :key="element.order"> 
+                <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+                {{element.name}}
+              </li> 
+            </transition-group>
+        </draggable>
+      </div>
+    </td>    
+    <td class="col-sm-2">
+      <div>
+        <draggable element="span" v-model="list3" :options="dragOptions" :move="onMove" class="list-group"> 
+            <transition-group name="no" class="list-group" tag="ul">
+              <li class="list-group-item" v-for="element in list3" :key="element.order"> 
+                <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+                {{element.name}}
+              </li> 
+            </transition-group>
+        </draggable>
+      </div>
+    </td>
+    <td></td>     
+    <td></td>     
+    <td></td>     
+    <td></td>        
+    </tr>
+    <tr>
+      <th scope="row">Stefan</th>
+      <td></td>     
+      <td></td>     
+      <td></td>     
+      <td></td>     
+      <td></td>     
+      <td></td>       
+      <td></td>             
+    </tr>
+    <tr>
+    <th scope="row">Martin</th>
+    <td class="col-sm-2">
+      <div>
+        <draggable element="span" v-model="list4" :options="dragOptions" :move="onMove" class="list-group"> 
+            <transition-group name="no" class="list-group" tag="ul">
+              <li class="list-group-item" v-for="element in list4" :key="element.order" :class="element.visible? '':''"> 
+                <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+                {{element.name}}
+              </li> 
+            </transition-group>
+        </draggable>
+      </div>
+    </td>
+    <td></td>     
+    <td></td>     
+    <td></td>     
+    <td></td>     
+    <td></td>     
+    <td></td>                 
+    </tr>
+  </tbody>
+</table>
+<!-- <pre>{{listString}}</pre> -->
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+const message = [ 'draggable', 'component', 'for' ]
+const listPending1 = [ 'draggable1', 'component1', 'for1' ]
+
+export default {
+  name: 'hello',
+  components: {
+    draggable,
+  },
+  data () {
+    return {
+      list: message.map( (name,index) => {return {name, order: index+1, fixed: false}; }),
+      listPending: listPending1.map( (name,index) => {return {name, order: index+1, fixed: false}; }),
+      list2:[],
+      list3:[],
+      list4:[],
+      list5:[  {
+                "name": "",
+                "order": 1000,
+                "fixed": false,
+                "visible": false
+      }],
+      editable:true,
+      isDragging: true,
+      delayedDragging:true
+    }
+  },
+  methods:{
+    orderList () {
+      this.list = this.list.sort((one,two) =>{return one.order-two.order; })
+    },
+    onMove ({relatedContext, draggedContext}) { 
+      const relatedElement = relatedContext.element;
+      const draggedElement = draggedContext.element;
+      // console.log(relatedElement)
+      return (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
+    },
+    onEnd(evt, originalEvent){
+      console.log(evt, originalEvent)
+    },    
+    test() {
+      console.log('test')
+    }
+  },
+  computed: {
+    dragOptions () {
+      return  {
+        animation: 0,
+        group: 'description',
+        disabled: !this.editable,
+        ghostClass: 'ghost'
+      };
+    },
+    listString(){
+      console.log(JSON.stringify(this.list, null, 2))
+      return JSON.stringify(this.list, null, 2);  
+    },
+    list2String(){
+      return JSON.stringify(this.list2, null, 2);  
+    },
+    list3String(){
+      return JSON.stringify(this.list3, null, 2);  
+    },
+    list4String(){
+      return JSON.stringify(this.list4, null, 2);  
+    }        
+  },
+  watch: {
+    isDragging (newValue) {
+      if (newValue){
+        this.delayedDragging= true        
+        return
+      }
+      this.$nextTick( () =>{
+           this.delayedDragging =false
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.table > tr > td {
+     vertical-align: top !important;
+}
+.flip-list-move {
+  transition: transform 0.5s;
+}
+
+.no-move {
+  transition: transform 0s;
+}
+
+.ghost {
+  opacity: .5;
+  background: #C8EBFB;
+}
+
+.hidden {
+  /* display: block */
+}
+
+.list-group {
+  min-height: 20px;
+}
+
+.list-group-item {
+  cursor: move;
+}
+
+.list-group-item i{
+  cursor: pointer;
+}
+</style>
+

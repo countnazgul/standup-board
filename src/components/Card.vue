3ca926d30e1b26a57d340f8b1d8af88e449f9e4c
Stefan Stoichev <stefan.stoichev@gmail.com>
2017-12-15 21:53:23 +0000

    getting there

---
+++ src/components/Card.vue
@@ -0,0 +1,68 @@
+<template>      
+  <li class="list-group-item" > 
+    <i :class="'fa fa-pencil'" @click="cardDetails" aria-hidden="true"></i>
+    {{element.name}} 
+  </li> 
+</template>
+
+<script>
+
+export default {
+  name: "card",
+  components: {
+    
+  },
+  props: ['element'],
+  data() {
+    return {
+    };
+  },
+  methods: {
+    cardDetails: function() {
+      this.element.name = this.element.name + ' BLAAAAAAAAAAAAAAAAAAAAAAAAA'
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+
+  }
+};
+</script>
+
+<style>
+.table > tr > td {
+  vertical-align: top !important;
+}
+.flip-list-move {
+  transition: transform 0.5s;
+}
+
+.no-move {
+  transition: transform 0s;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.hidden {
+  /* display: block */
+}
+
+.list-group {
+  min-height: 42px;
+  background-color: #efefef;
+}
+
+.list-group-item {
+  cursor: move;
+}
+
+.list-group-item i {
+  cursor: pointer;
+}
+</style>
+
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,87 +1,136 @@
 <template>
   <div class="fluid">
-<!-- fluid container
-On hold | Proposal | Discovery | Design | Delivery | QA | Suuport
--->
-      <div>
-        <draggable element="span" v-model="listPending" :options="dragOptions" :move="onMove" class="list-group" @end="onEnd"> 
-            <transition-group name="no" class="list-group" tag="ul">
-              <li class="list-group-item" v-for="element in listPending" :key="element.order"> 
-                <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
-                {{element.name}}
-              </li> 
-            </transition-group>
-        </draggable>
-      </div>
+    <div>
+      <draggable element="span" v-model="cards.pending" :options="dragOptions" :move="onMove" class="list-group" @end="onEnd"> 
+          <transition-group name="no" class="list-group" tag="ul">
+            <li class="list-group-item" v-for="element in cards.pending" :key="element.order"> 
+              <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+              {{element.name}}
+            </li> 
+          </transition-group>
+      </draggable>
+    </div>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th v-for="column in columns" >{{column}}</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="card in cards">
-      <th scope="row">{{card.name}}</th>
-      <td class="col-sm-2">
-        <div>
-          <draggable element="span" v-model="list" :options="dragOptions" :move="onMove" class="list-group"> 
-              <transition-group name="no" class="list-group" tag="ul">
-                <li class="list-group-item" v-for="element in card.personalCards.onhold" :key="element.order"> 
-                  <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
-                  {{element.name}}
-                </li> 
-              </transition-group>
-          </draggable>
-        </div>
-      </td>
+<!-- <li class="list-group-item" v-for="element in card.personalCards.onhold" :key="element.order"> 
+  <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+  {{element.name}}
+</li>  -->
 
-      <td class="col-sm-2">
-        <div>
-          <draggable element="span" v-model="list" :options="dragOptions" :move="onMove" class="list-group"> 
-              <transition-group name="no" class="list-group" tag="ul">
-                <li class="list-group-item" v-for="element in card.personalCards.proposal" :key="element.order"> 
-                  <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
-                  {{element.name}}
-                </li> 
-              </transition-group>
-          </draggable>
-        </div>
-      </td>
+    <table class="table">
+      <thead>
+        <tr>
+          <th v-for="column in columns" >{{column}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="card in cards.assigned">
+          <th scope="row">{{card.name}}</th>
+          <td class="col-sm-2">
+            <div>
+              <draggable element="span" v-model="card.personalCards.onhold" :options="dragOptions" :move="onMove" class="list-group" @end="onEnd"> 
+                  <transition-group name="no" class="list-group" tag="ul">
+                    <card  v-for="element in card.personalCards.onhold" v-bind:key="element.order" :element="element"></card>
+                  </transition-group>
+              </draggable>
+            </div>
+          </td>
 
-      <td class="col-sm-2">
-        <div>
-          <draggable element="span" v-model="list" :options="dragOptions" :move="onMove" class="list-group"> 
-              <transition-group name="no" class="list-group" tag="ul">
-                <li class="list-group-item" v-for="element in card.personalCards.discovery" :key="element.order"> 
-                  <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
-                  {{element.name}}
-                </li> 
-              </transition-group>
-          </draggable>
-        </div>
-      </td>      
+          <td class="col-sm-2">
+            <div>
+              <draggable element="span" v-model="card.personalCards.proposal" :options="dragOptions" :move="onMove" class="list-group" @end="onEnd"> 
+                  <transition-group name="no" class="list-group" tag="ul">
+                    <li class="list-group-item" v-for="element in card.personalCards.proposal" :key="element.order"> 
+                      <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+                      {{element.name}}
+                    </li> 
+                  </transition-group>
+              </draggable>
+            </div>
+          </td>
 
-    </tr>
-  </tbody>
-</table>
+          <td class="col-sm-2">
+            <div>
+              <draggable element="span" v-model="card.personalCards.discovery" :options="dragOptions" :move="onMove" class="list-group" @end="onEnd"> 
+                  <transition-group name="no" class="list-group" tag="ul">
+                    <li class="list-group-item" v-for="element in card.personalCards.discovery" :key="element.order"> 
+                      <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+                      {{element.name}}
+                    </li> 
+                  </transition-group>
+              </draggable>
+            </div>
+          </td>  
+
+          <td class="col-sm-2">
+            <div>
+              <draggable element="span" v-model="card.personalCards.design" :options="dragOptions" :move="onMove" class="list-group" @end="onEnd"> 
+                  <transition-group name="no" class="list-group" tag="ul">
+                    <li class="list-group-item" v-for="element in card.personalCards.design" :key="element.order"> 
+                      <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+                      {{element.name}}
+                    </li> 
+                  </transition-group>
+              </draggable>
+            </div>
+          </td>   
+
+          <td class="col-sm-2">
+            <div>
+              <draggable element="span" v-model="card.personalCards.delivery" :options="dragOptions" :move="onMove" class="list-group" @end="onEnd"> 
+                  <transition-group name="no" class="list-group" tag="ul">
+                    <li class="list-group-item" v-for="element in card.personalCards.delivery" :key="element.order"> 
+                      <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+                      {{element.name}}
+                    </li> 
+                  </transition-group>
+              </draggable>
+            </div>
+          </td>  
+
+          <td class="col-sm-2">
+            <div>
+              <draggable element="span" v-model="card.personalCards.qa" :options="dragOptions" :move="onMove" class="list-group" @end="onEnd"> 
+                  <transition-group name="no" class="list-group" tag="ul">
+                    <li class="list-group-item" v-for="element in card.personalCards.qa" :key="element.order"> 
+                      <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+                      {{element.name}}
+                    </li> 
+                  </transition-group>
+              </draggable>
+            </div>
+          </td>  
+
+          <td class="col-sm-2">
+            <div>
+              <draggable element="span" v-model="card.personalCards.support" :options="dragOptions" :move="onMove" class="list-group" @end="onEnd"> 
+                  <transition-group name="no" class="list-group" tag="ul">
+                    <li class="list-group-item" v-for="element in card.personalCards.support" :key="element.order"> 
+                      <i :class="element.fixed? 'fa fa-pencil' : 'fa fa-pencil'" @click="test" aria-hidden="true"></i>
+                      {{element.name}}
+                    </li> 
+                  </transition-group>
+              </draggable>
+            </div>
+          </td>                                              
+
+        </tr>
+      </tbody>
+    </table>
 <!-- <pre>{{listString}}</pre> -->
   </div>
 </template>
 
 <script>
 import draggable from "vuedraggable";
-const message = ["draggable", "component", "for"];
-const listPending1 = ["draggable1", "component1", "for1"];
+import card from "./Card";
 
 export default {
   name: "hello",
   components: {
-    draggable
+    draggable, card
   },
   data() {
     return {
-      // members: ['Piers', 'Stefan', 'Martin', 'Dean'],
       columns: [
         "Member",
         "On hold",
@@ -92,73 +141,99 @@ export default {
         "QA",
         "Support"
       ],
-      cards: [
-        {
-          name: "Piers",
-          personalCards: {
-            onhold: [
-              {
-                name: "Card 1",
-                order: 1,
-                fixed: false
-              },
-              {
-                name: "Card 2",
-                order: 2,
-                fixed: false
-              },
-              {
-                name: "Card 3",
-                order: 3,
-                fixed: false
-              }
-            ],
-            proposal: [],
-            discovery: [
-              {
-                name: "Card 4",
-                order: 1,
-                fixed: false
-              },
-              {
-                name: "Card 5",
-                order: 2,
-                fixed: false
-              },
-              {
-                name: "Card 6",
-                order: 3,
-                fixed: false
-              }
-            ]
+      cards: {
+        pending: [
+          {
+            name: "Card 101",
+            order: 1,
+            fixed: false,
+            details: "123"
+          },
+          {
+            name: "Card 102",
+            order: 2,
+            fixed: false
+          },
+          {
+            name: "Card 103",
+            order: 3,
+            fixed: false
           }
-        },
-        {
-          name: "Stefan",
-          personalCards: []
-        },
-        {
-          name: "Dean",
-          personalCards: []
-        }
-      ],
-      list: message.map((name, index) => {
-        return { name, order: index + 1, fixed: false };
-      }),
-      listPending: listPending1.map((name, index) => {
-        return { name, order: index + 1, fixed: false };
-      }),
-      list2: [],
-      list3: [],
-      list4: [],
-      list5: [
-        {
-          name: "",
-          order: 1000,
-          fixed: false,
-          visible: false
-        }
-      ],
+        ],
+        assigned: [
+          {
+            name: "Piers",
+            personalCards: {
+              onhold: [
+                {
+                  name: "Card 1",
+                  order: 1,
+                  fixed: false,
+                  details: "123"
+                },
+                {
+                  name: "Card 2",
+                  order: 2,
+                  fixed: false
+                },
+                {
+                  name: "Card 3",
+                  order: 3,
+                  fixed: false
+                }
+              ],
+              proposal: [],
+              discovery: [
+                {
+                  name: "Card 4",
+                  order: 1,
+                  fixed: false
+                },
+                {
+                  name: "Card 5",
+                  order: 2,
+                  fixed: false
+                },
+                {
+                  name: "Card 6",
+                  order: 3,
+                  fixed: false
+                }
+              ],
+              design: [],
+              delivery: [],
+              qa: [],
+              support: []
+            }
+          },
+          {
+            name: "Stefan",
+            personalCards: {
+              onhold: [],
+              proposal: [],
+              discovery: [],
+              design: [],
+              delivery: [],
+              qa: [],
+              support: []
+              
+            }
+          },
+          {
+            name: "Dean",
+            personalCards: {
+              onhold: [],
+              proposal: [],
+              discovery: [],
+              design: [],
+              delivery: [],
+              qa: [],
+              support: []
+            }
+          }
+        ]
+      },
+      list: {},
       editable: true,
       isDragging: true,
       delayedDragging: true
@@ -171,18 +246,22 @@ export default {
       });
     },
     onMove({ relatedContext, draggedContext }) {
+      // console.log(relatedContext);
+      // console.log(draggedContext);
+      // console.log("test");
       const relatedElement = relatedContext.element;
       const draggedElement = draggedContext.element;
-      // console.log(relatedElement)
+
       return (
         (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
       );
     },
     onEnd(evt, originalEvent) {
-      console.log(evt, originalEvent);
+      // console.log("test123");
+      //console.log(evt, originalEvent);
     },
-    test() {
-      console.log("test");
+    test(evt) {
+      console.log(this);
     }
   },
   computed: {
@@ -193,19 +272,6 @@ export default {
         disabled: !this.editable,
         ghostClass: "ghost"
       };
-    },
-    listString() {
-      console.log(JSON.stringify(this.list, null, 2));
-      return JSON.stringify(this.list, null, 2);
-    },
-    list2String() {
-      return JSON.stringify(this.list2, null, 2);
-    },
-    list3String() {
-      return JSON.stringify(this.list3, null, 2);
-    },
-    list4String() {
-      return JSON.stringify(this.list4, null, 2);
     }
   },
   watch: {
@@ -245,7 +311,7 @@ export default {
 
 .list-group {
   min-height: 42px;
-  background-color: lightgray;
+  background-color: #efefef;
 }
 
 .list-group-item {
